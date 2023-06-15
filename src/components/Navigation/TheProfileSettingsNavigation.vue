aaeee5dc26449275b6d8b3c14035599086024261
osakac <shinmoriof@gmail.com>
2023-06-15 11:38:12 +0400

    improved profile settings page

--- src/components/Navigation/TheProfileSettingsNavigation.vue
+++ src/components/Navigation/TheProfileSettingsNavigation.vue
@@ -1,19 +1,19 @@
 <template>
 	<ul class="profile-settings__navigation">
-		<li class="profile-settings__item">
-			<router-link class="profile-settings__item-btn" to="/profile-settings">
+		<li class="profile-settings__nav-item">
+			<router-link class="profile-settings__nav-item-btn" to="/profile-settings">
 				<i class="profile-settings__nav-icon fa-solid fa-pen"></i>
 				Profile
 			</router-link>
 		</li>
-		<li class="profile-settings__item">
-			<router-link class="profile-settings__item-btn" to="/profile-settings-account">
+		<li class="profile-settings__nav-item">
+			<router-link class="profile-settings__nav-item-btn" to="/profile-settings-account">
 				<i class="profile-settings__nav-icon fa-solid fa-user"></i>
 				Account
 			</router-link>
 		</li>
-		<li class="profile-settings__item">
-			<router-link class="profile-settings__item-btn" to="/profile-settings-password">
+		<li class="profile-settings__nav-item">
+			<router-link class="profile-settings__nav-item-btn" to="/profile-settings-password">
 				<i class="profile-settings__nav-icon fa-solid fa-lock"></i>
 				Password
 			</router-link>
--- src/views/ProfileSettingsPage.vue
+++ src/views/ProfileSettingsPage.vue
@@ -1,5 +1,22 @@
 <template>
-	<TheProfileSettingsNavigation />
+	<div class="profile-settings__wrapper">
+		<TheProfileSettingsNavigation />
+
+		<div class="profile-settings__content">
+			<h2 class="profile-settings__title">Profile</h2>
+			<div class="profile-settings__field-block">
+				<span class="profile-settings__field-label">Avatar</span>
+				<div class="profile-settings__avatar-block">
+					<img class="profile-settings__avatar" src="https://gas-kvas.com/uploads/posts/2023-01/1673393665_gas-kvas-com-p-risunki-na-avatarku-anime-15.jpg" alt="avatar">
+					<input class="profile-settings__avatar-input" type="file" accept=".jpg, .jpeg, .png, .gif">
+				</div>
+			</div>
+			<div class="profile-settings__field-block">
+				<span class="profile-settings__field-label">Display name</span>
+				<input type="text">
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
